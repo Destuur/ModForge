183660f4771e6d3ef6accc5a994209caef8e493e
Destuur <155296556+Destuur@users.noreply.github.com>
2025-05-28 16:10:51 +0200

    Update AttributeFactory.cs

--- KCD2.ModForge.Shared/AttributeFactory.cs
+++ KCD2.ModForge.Shared/AttributeFactory.cs
@@ -10,6 +10,31 @@ namespace KCD2.ModForge.Shared
 	// TODO: Factory fertig bauen
 	public static class AttributeFactory
 	{
+		private static Func<string, object, ImmediateSubscriptionTrigger> NewMethod(string name, string value)
+{
+    string typeName = string.Concat(
+        name.Split('-').Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1))
+    );
+    // Das aktuelle Assembly (oder ein beliebiges anderes)
+    Assembly assembly = Assembly.GetExecutingAssembly(); // oder Assembly.Load("Dein.Assembly.Name")
+    // Alle aktuell geladenen Assemblies durchsuchen
+    Type? foundType = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(a => a.GetTypes())
+        .FirstOrDefault(t => t.Name == typeName);
+
+    var typeExpression = Expression.Parameter(typeof(string), nameof(name));
+    var valueExpression = Expression.Parameter(typeof(object), nameof(value));
+
+    var constructor = foundType!.GetConstructors().FirstOrDefault();
+
+    var newExpression = Expression.New(constructor!, typeExpression, valueExpression);
+
+    var lambda = Expression.Lambda<Func<string, object, ImmediateSubscriptionTrigger>>(newExpression, typeExpression, valueExpression);
+    var func = lambda.Compile();
+
+    return func!;
+}
+		
 		public static IAttribute CreateAttribute(string type, object value)
 		{
 			return type switch
