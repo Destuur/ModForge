9cf5563115670ed3e7d81169e4a45797fd27f69f
Michael <m.weichart@gmail.com>
2025-05-28 21:16:26 +0200

    implemented AttributeFactory

--- KCD2.ModForge.Shared/AttributeFactory.cs
+++ KCD2.ModForge.Shared/AttributeFactory.cs
@@ -1,76 +1,40 @@
 ﻿using KCD2.ModForge.Shared.Models.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace KCD2.ModForge.Shared
 {
 	// TODO: Factory fertig bauen
 	public static class AttributeFactory
 	{
-		private static Func<string, object, ImmediateSubscriptionTrigger> NewMethod(string name, string value)
-{
-    string typeName = string.Concat(
-        name.Split('-').Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1))
-    );
-    // Das aktuelle Assembly (oder ein beliebiges anderes)
-    Assembly assembly = Assembly.GetExecutingAssembly(); // oder Assembly.Load("Dein.Assembly.Name")
-    // Alle aktuell geladenen Assemblies durchsuchen
-    Type? foundType = AppDomain.CurrentDomain.GetAssemblies()
-        .SelectMany(a => a.GetTypes())
-        .FirstOrDefault(t => t.Name == typeName);
-
-    var typeExpression = Expression.Parameter(typeof(string), nameof(name));
-    var valueExpression = Expression.Parameter(typeof(object), nameof(value));
-
-    var constructor = foundType!.GetConstructors().FirstOrDefault();
-
-    var newExpression = Expression.New(constructor!, typeExpression, valueExpression);
-
-    var lambda = Expression.Lambda<Func<string, object, ImmediateSubscriptionTrigger>>(newExpression, typeExpression, valueExpression);
-    var func = lambda.Compile();
-
-    return func!;
-}
-		
-		public static IAttribute CreateAttribute(string type, object value)
+		private static Func<string, object, IAttribute> BuildAttribute(string name, object value)
 		{
-			return type switch
-			{
-				"autolearnable" => new Autolearnable(type, value),
-				"buff_class_id" => new BuffClassId(type, value),
-				"buff_exclusivity_id" => new BuffExclusivityId(type, value),
-				"buff_id" => new BuffId(type, value),
-				"buff_lifetime_id" => new BuffLifetimeId(type, value),
-				"buff_name" => new BuffName(type, value),
-				"buff_params" => new BuffParams(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				//"autolearnable" => new Autolearnable(type, value),
-				_ => throw new ArgumentException("Attribute unkown")
-			};
+			string typeName = string.Concat(
+				name.Split('_').Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1))
+			);
+			// Das aktuelle Assembly (oder ein beliebiges anderes)
+			var assembly = Assembly.GetExecutingAssembly(); // oder Assembly.Load("Dein.Assembly.Name")
+															// Alle aktuell geladenen Assemblies durchsuchen
+			var foundType = AppDomain.CurrentDomain.GetAssemblies()
+				.SelectMany(a => a.GetTypes())
+				.FirstOrDefault(t => t.Name == typeName);
+
+			var typeExpression = Expression.Parameter(typeof(string), nameof(name));
+			var valueExpression = Expression.Parameter(typeof(object), nameof(value));
+
+			var constructor = foundType!.GetConstructors().FirstOrDefault();
+
+			var newExpression = Expression.New(constructor!, typeExpression, valueExpression);
+
+			var lambda = Expression.Lambda<Func<string, object, IAttribute>>(newExpression, typeExpression, valueExpression);
+			var func = lambda.Compile();
+
+			return func!;
+		}
+
+		public static IAttribute CreateAttribute(string name, object value)
+		{
+			return BuildAttribute(name, value).Invoke(name, value);
 		}
 	}
 }
--- KCD2.ModForge.Test/Program.cs
+++ KCD2.ModForge.Test/Program.cs
@@ -11,7 +11,11 @@ namespace KCD2.ModForge.Test
 			var name = buffexclusive.Name;
 			var value = buffexclusive.Value;
 
-			var attribute = AttributeFactory.CreateAttribute("buff_params", "ewd*0.85,dew*2");
+			// Wubba lubba dub dub
+			var attribute1 = AttributeFactory.CreateAttribute("buff_params", "ewd*0.85,dew*2");
+			var attribute2 = AttributeFactory.CreateAttribute("buff_ui_visibility_id", "1");
+			var attribute3 = AttributeFactory.CreateAttribute("level", "12");
+			var attribute4 = AttributeFactory.CreateAttribute("autolearnable", "true");
 
 			var buffParams = new BuffParams("test", "ewd*0.85,dew*2");
 			var buffParamsName = buffParams.Name;
