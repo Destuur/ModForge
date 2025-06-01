2fc25e56dee054225de63b2bc1a325896c919161
Michael Weichart <michael.weichart@ximaj.com>
2025-06-01 11:49:03 +0200

    Start to implement BuffParamComponent

--- KCD2.ModForge.UI/Components/AttributeComponents/BuffParamsAttribute.razor.cs
+++ KCD2.ModForge.UI/Components/AttributeComponents/BuffParamsAttribute.razor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KCD2.ModForge.Shared.Models.Attributes;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -8,21 +10,29 @@ using System.Threading.Tasks;
 
 namespace KCD2.ModForge.UI.Components.AttributeComponents
 {
-	public partial class BuffParamsAttribute
-	{
-		private string FormatLabel(string raw)
-		{
-			if (string.IsNullOrWhiteSpace(raw))
-				return string.Empty;
+    public partial class BuffParamsAttribute
+    {
+        [CascadingParameter]
+        public IAttribute? Attribute { get; set; }
+        IList<BuffParam>? CurrentValues
+        {
+            get => (IList<BuffParam>)Attribute!.Value;
+            set => Attribute!.Value = value;
+        }
 
-			// Unterstriche durch Leerzeichen ersetzen
-			string noUnderscores = raw.Replace("_", " ");
+        private string FormatLabel(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return string.Empty;
 
-			// CamelCase trennen
-			string withSpaces = Regex.Replace(noUnderscores, "(?<!^)([A-Z])", " $1");
+            // Unterstriche durch Leerzeichen ersetzen
+            string noUnderscores = raw.Replace("_", " ");
 
-			// Jeden Wortanfang großschreiben
-			return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(withSpaces.ToLower());
-		}
-	}
+            // CamelCase trennen
+            string withSpaces = Regex.Replace(noUnderscores, "(?<!^)([A-Z])", " $1");
+
+            // Jeden Wortanfang großschreiben
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(withSpaces.ToLower());
+        }
+    }
 }
