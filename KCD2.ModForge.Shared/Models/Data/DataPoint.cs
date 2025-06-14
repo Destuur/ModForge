8823b9fcffe5e8cae20f35c31785c90d6a52c19f
Destuur <155296556+Destuur@users.noreply.github.com>
2025-06-14 23:42:35 +0200

    Fixed Import crash

--- KCD2.ModForge.Shared/Models/Data/DataPoint.cs
+++ KCD2.ModForge.Shared/Models/Data/DataPoint.cs
@@ -2,9 +2,9 @@
 {
 	public class DataPoint : IDataPoint
 	{
-		public DataPoint(string folder, string endpoint, Type type)
+		public DataPoint(string path, string endpoint, Type type)
 		{
-			Path = folder;
+			Path = path;
 			Endpoint = endpoint;
 			Type = type;
 		}
--- KCD2.ModForge.Shared/ToolResources.cs
+++ KCD2.ModForge.Shared/ToolResources.cs
@@ -1,4 +1,6 @@
-﻿namespace KCD2.ModForge.Shared
+﻿using KCD2.ModForge.Shared.Models.ModItems;
+
+namespace KCD2.ModForge.Shared
 {
 	public class ToolResources
 	{
@@ -9,6 +11,29 @@
 
 		public static ToolResources Keys { get; set; } = new ToolResources();
 
+		#region Valid Methods
+		public Dictionary<Type, Dictionary<string, string>> Endpoints() => new()
+		{
+			{ typeof(Perk), new()
+				{
+					{ "perk__combat", Path.Combine("Data", "Tables.pak")},
+					{ "perk__hardcore", Path.Combine("Data", "Tables.pak")},
+					{ "perk__kcd2", Path.Combine("Data", "Tables.pak")},
+				}
+			},
+			{ typeof(Buff), new()
+				{
+					{  "buff", Path.Combine("Data", "Tables.pak")},
+					{  "buff__alchemy", Path.Combine("Data", "Tables.pak")},
+					{  "buff__perk", Path.Combine("Data", "Tables.pak")},
+					{  "buff__perk_hardcore", Path.Combine("Data", "Tables.pak")},
+					{  "buff__perk_kcd1", Path.Combine("Data", "Tables.pak")},
+				}
+			},
+		};
+		#endregion
+
+		#region Test Methods
 		public string ModId() => "test_mod";
 		public string TablesPath() => "G:\\SteamLibrary\\steamapps\\common\\KingdomComeDeliverance2\\Data\\Tables.pak";
 		public string GermanLocalizationPath() => "G:\\SteamLibrary\\steamapps\\common\\KingdomComeDeliverance2\\Localization\\German_xml.pak";
@@ -17,5 +42,6 @@
 		public string ModPath() => "G:\\SteamLibrary\\steamapps\\common\\KCD2Mod\\Mods";
 		public string KCD2ModsPath() => "G:\\SteamLibrary\\steamapps\\common\\KingdomComeDeliverance2\\Mods";
 		public string GameDirectory() => "G:\\SteamLibrary\\steamapps\\common\\KingdomComeDeliverance2";
+		#endregion
 	}
 }
